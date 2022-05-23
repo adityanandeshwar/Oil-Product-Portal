94850a6ee192315cc5cba2927c1a1053b1cdda0c
Aditya Nandeshwar <57413649+adityanandeshwar@users.noreply.github.com>
2022-05-23 23:39:55 +0530

    Add files via upload

---
+++ book_shop/about1.php
@@ -0,0 +1,9 @@
+<section class="py-5">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-body">
+                <?php include "_index.html" ?>
+            </div>
+        </div>
+    </div>
+</section>
