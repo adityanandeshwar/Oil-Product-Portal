671c8130fb5bd03edb500539744f9c1c9bb93f04
Aditya Nandeshwar <57413649+adityanandeshwar@users.noreply.github.com>
2022-05-23 23:38:19 +0530

    Delete project directory

--- project/about1.php
+++
@@ -1,9 +0,0 @@
-<section class="py-5">
-    <div class="container">
-        <div class="card rounded-0">
-            <div class="card-body">
-                <?php include "_index.html" ?>
-            </div>
-        </div>
-    </div>
-</section>
